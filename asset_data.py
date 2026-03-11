c729a3eb083d932dc84e94221ef80d0f152be8ec
T-Nai <tananusin@gmail.com>
2026-03-11 10:41:40 +0700

    Update asset_data.py

--- asset_data.py
+++ asset_data.py
@@ -18,30 +18,15 @@ class AssetData:
     value_thb: Optional[float] = None
     weight: Optional[float] = None
 
-    # Proportion
-    target: Optional[float] = None
-
-    # Position Size
-    drift: Optional[float] = None
-    drift_pct: Optional[float] = None
-    position_size: Optional[str] = None
-
     # Price Change
     high_52w: Optional[float] = None
     low_52w: Optional[float] = None
     low_3y: Optional[float] = None
-    drop_1y: Optional[float] = None
-    gain_1y: Optional[float] = None
-    gain_3y: Optional[float] = None
-    price_change: Optional[str] = None
 
     # P/E Signal
     pe_ratio: Optional[float] = None                    # Trailing P/E ratio
     pe_p25: Optional[float] = None                      # 25th percentile P/E
     pe_p75: Optional[float] = None                      # 75th percentile P/E
-    pe_signal: Optional[str] = None
 
     # Dividend Yield Signal
     dividend_yield: Optional[float] = None              # Trailing dividend yield
-    dividend_yield_offset: Optional[float] = None
-    dividend_yield_signal: Optional[str] = None
